2545549db8ec828d727ac26e3d4f4fe10ba2256a
Eric Pratt <ericpratt86@gmail.com>
2018-06-13 16:02:45 -0700

    Create layout

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-
+const path = require('path');
 const app = express();
+const port = process.env.PORT || 8000;
 
+
+app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +14,6 @@ app.get('/', (req, res) => {
   res.render('index', {title: 'TA Marsh | Roofing'});
 });
 
-app.listen(8000, () => {
-  console.log('server is running on 8000');
+app.listen(port, () => {
+  console.log(`Server is running on: ${port}`);
 });
