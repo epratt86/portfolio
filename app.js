946a5d78368418efd2e5bfb8aa1d2492912f5564
Eric Pratt <ericpratt86@gmail.com>
2018-06-21 11:46:38 -0700

    Add inquery form

--- app.js
+++ app.js
@@ -1,21 +1,74 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const mongoose = require('mongoose');
 const path = require('path');
-const app = express();
-const port = process.env.PORT || 8000;
+const {User} = require('./models/user');
+const _ = require('lodash');
 
+const port = process.env.PORT;
+const app = express();
+const sessionStore = new session.MemoryStore;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MARSH_DB);
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_PARSER));
+//express session middleware
+app.use(session({
+  secret: process.env.SESSION,
+  resave: 'true',
+  store: sessionStore,
+  saveUninitialized: true,
+  cookie: {maxAge: 60000 }
+}));
+
+app.use(flash());
+
+//lets us know who is logged in. keep above routes
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error = req.flash("error");
+   res.locals.success = req.flash("success");
+   next();
+});
 
 app.get('/', (req, res) => {
   res.render('index', {title: 'Home | TA Marsh'});
 });
 
+app.post('/user', (req, res) => {
+  let body = _.pick(req.body, ['name', 'email', 'phone', 'comment']);
+  let user = new User(body);
+
+
+  user.save((err) => {
+    if (err) {
+      req.flash('error', 'Oops! Something went wrong with your request.');
+    } else {
+      req.flash('success', 'Your message has been sent.');
+    }
+    res.redirect('/');
+  });
+});
+
 app.get('/reviews', (req, res) => {
-  res.render('reviews', {title: 'Reviews | TA Marsh'})
+  res.render('reviews', {title: 'Reviews | TA Marsh'});
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {title: 'Contact | TA Marsh'});
 });
 
 app.listen(port, () => {
