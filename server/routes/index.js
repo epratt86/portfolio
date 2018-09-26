42e11c6fbbad15b87866e0e1a7f0fc02fc5cf20e
Eric Pratt <ericpratt86@gmail.com>
2018-09-26 14:58:01 -0700

    created about me

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 //landing page
 router.get('/', (req, res) => {
-  res.render('landing', {title: 'epratt | Developer', layout: 'no-header'});
+  res.render('landing', {title: 'Welcome | ePratt.net'});
+});
+
+router.get('/me', (req, res) => {
+  res.render('me', {title: 'About | ePratt.net'});
 });
 
 module.exports = router;
