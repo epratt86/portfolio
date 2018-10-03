b2a525e127f207bc6edf5faccdedf457cbbcdbf9
Eric Pratt <ericpratt86@gmail.com>
2018-10-03 10:38:58 -0700

    add portfolio page

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,17 @@ router.get('/', (req, res) => {
   res.render('landing', {title: 'Welcome | ePratt.net'});
 });
 
+//about me
 router.get('/me', (req, res) => {
   res.render('me', {title: 'About | ePratt.net'});
 });
 
+//portfolio
+router.get('/portfolio', (req, res) => {
+  res.render('portfolio', {title: 'Portfolio | ePratt.net'});
+});
+
+//user requests info
 router.post('/signup', (req, res) => {
   const newUser = new User({
     name: req.body.name,
